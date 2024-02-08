1d884ec8331fdf55b2e30d82e66671a9d362f419
Edralli <vasselin.adrien@gmail.com>
2024-02-08 15:26:14 +0100

    Update run_workflow.sh

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "Hello, World!"
 
-source C:/Users/Vasse/Bureau/ProjetAutomatisationAppel/run_workflow.sh
+source C:/Users/Vasse/Bureau/ProjetAutomatisationAppel/config/db_config.sh
+
+PROJECT_PATH='C:/Users/Vasse/Bureau/ProjetAutomatisationAppel'
+
+source $PROJECT_PATH/venv/bin/activate
+
+python $PROJECT_PATH/Scripts/extract_data.py
+python $PROJECT_PATH/scripts/convert_to_xml.py
+python $PROJECT_PATH/scripts/send_to_app.py
+
+deactivate
+
+echo "Workflow completed at $(date)" >> $PROJECT_PATH/logs/workflow.log
